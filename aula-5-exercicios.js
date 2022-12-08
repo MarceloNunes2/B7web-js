252123ced6a6d5b76e0de8641b4ae409e4949a59
marcelonunes2 <mklelo1990@gmail.com>
2022-12-08 05:17:28 -0300

    aaaa

---
+++ aula-5-exercicios.js
@@ -0,0 +1,19 @@
+/* 1 Crie uma variável para armazenar o nome
+do carro e ponha o valor ferrari. E exiba-a
+*/
+
+let carro = "Ferrari"
+console.log(carro)
+
+/* 2 Crie uma variável para armazenar o preço do bolo, que custa $19,99
+E exiba-a*/
+
+let preco = 19.99
+console.log(preco)
+
+/* Corrija o código a seguir e exiba-o
+let "cidade" = São Paulo
+*/
+
+let cidade = "São Paulo"
+console.log(cidade)
--- tipos-de-dados.js
+++ tipos-de-dados.js
@@ -1,4 +1,7 @@
-let nome = "Marcelo"
-let idade = 90
-let logado = true
-let deslogado = false
+let nome = "Marcelo" //String
+let idade = 90  //Number
+let logado = true //Booleam
+let deslogado = false  //Booleam
+let lista = ["ovo","leite","fermento"] //Array
+let nomecompleto = {nome:"Marcelo", idade:25} //Object
+
