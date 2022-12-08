9647623f0128187cf818a724714aa1913cb71ca0
marcelonunes2 <mklelo1990@gmail.com>
2022-12-08 14:04:34 -0300

    AAA

---
+++ aula-7-let-const-char.js
@@ -0,0 +1,12 @@
+//Usando o let e var, vão funcionar no mesmo contexto
+
+let nome = "Marcelo"
+console.log(nome)
+nome = "Pedro"
+console.log(nome)
+
+// Usando Const uma constante não pode ser alterada.
+
+const names = "Marcelo"
+console.log(names)
+
---
+++ aula-8-operações-em-variaveis.js
@@ -0,0 +1,10 @@
+let x = 2
+
+x = x + 8
+
+console.log(x) 
+
+let nome = "Marcelo"
+let sobrenome = "Numes"
+let nomecompleto = nome + " " + sobrenome
+console.log (nomecompleto)
